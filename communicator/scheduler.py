0057a8e303551b0611a09c848359aa2d7226aee5
Dan Funk <daniel.h.funk@gmail.com>
2020-09-24 11:51:50 -0400

    Test the scheduler, and see if it works.

--- communicator/scheduler.py
+++ communicator/scheduler.py
@@ -1,17 +1,22 @@
-import os
-from apscheduler.schedulers.blocking import BlockingScheduler
 from communicator import db
+import time
+import atexit
 
-if __name__ == '__main__':
-    scheduler = BlockingScheduler()
-    scheduler.add_jobstore('sqlalchemy', url=db.engine.url)
-    scheduler.add_job(
-        'communicator.api.admin:update_data', 'interval', minutes=10,
-         id='update_data', replace_existing=True
-    )
+from apscheduler.schedulers.background import BackgroundScheduler
 
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-    try:
-        scheduler.start()
-    except (KeyboardInterrupt, SystemExit):
-        pass
+
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_jobstore('sqlalchemy', url=db.engine.url)
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
+scheduler.add_job(
+    'communicator.api.admin:update_data', 'interval', minutes=10, id='update_data', replace_existing=True
+)
+
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
