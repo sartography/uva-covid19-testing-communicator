d6edb3f129db8b0d4cadb82ae185a3171848a6d9
Dan <daniel.h.funk@gmail.com>
2020-10-26 17:24:51 -0400

    cleaning up the scheduler, so it can check for new files every hour, but only send out notifications between 1 and 2:30.

--- communicator/scheduler.py
+++ communicator/scheduler.py
@@ -12,38 +12,43 @@ from communicator.api import admin
 
 
 def within_notification_window():
-    """Where 'reasaonable' is between 8am and 10pm. """
     tz = pytz.timezone('US/Eastern')
     now = (datetime.now(tz))
     one_pm = (datetime.now(tz).replace(hour=13, minute=0, second=0, microsecond=0))
-    two_pm = (datetime.now(tz).replace(hour=14, minute=0, second=0, microsecond=0))
+    two_pm = (datetime.now(tz).replace(hour=14, minute=30, second=0, microsecond=0))
     return one_pm <= now <= two_pm
 
 
-def update_and_notify():
+def update():
     with app.app_context():
         if app.config['RUN_SCHEDULED_TASKS']:
             admin._update_data()
-            if within_notification_window():
-                app.logger.info("Within Notification Window, sending messages.")
-                admin._notify_by_email()
-                admin._notify_by_text()
-            else:
-                app.logger.info("Not within the notification window, not sending messages.")
+
+
+def notify():
+    with app.app_context():
+        if within_notification_window():
+            app.logger.info("Within Notification Window, sending messages.")
+            admin._notify_by_email()
+            admin._notify_by_text()
+        else:
+            app.logger.info("Not within the notification window, not sending messages.")
 
 
 if app.config['RUN_SCHEDULED_TASKS']:
     scheduler = BackgroundScheduler()
     scheduler.add_jobstore('sqlalchemy', url=db.engine.url)
     scheduler.add_job(
-        update_and_notify, 'interval', minutes=app.config['SCHEDULED_TASK_MINUTES'],
-        id='update_data', replace_existing=True
+        update, 'interval', minutes=60, id='update_data', replace_existing=True
     )
-
+    scheduler.add_job(
+        notify, 'interval', minutes=app.config['SCHEDULED_TASK_MINUTES'],
+        id='notify', replace_existing=True
+    )
     scheduler.start()
 
     # Shut down the scheduler when exiting the app
     atexit.register(lambda: scheduler.shutdown())
 else:
     app.logger.info("Currently not running scheduled tasks RUN_SCHEDULED_TASKS"
-                            " is set to false in configuration.")
+                    " is set to false in configuration.")
