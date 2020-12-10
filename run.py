339d4655931e7497bb62b1569124de1b3467d981
Nile Walker <niwal7@morgan.edu>
2020-12-10 08:16:01 -0500

    Added Random Data

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from communicator import app
 
-if __name__ == "__main__":
-    flask_port = app.config['FLASK_PORT']
-    app.run(host='0.0.0.0', port=flask_port)
+# if __name__ == "__main__":
+#     flask_port = app.config['FLASK_PORT']
+#     app.run(host='0.0.0.0', port=flask_port)
