8339c691ec49af12305246a659ab0ebe70416f56
Nile Walker <niwal7@morgan.edu>
2021-02-15 17:53:31 -0500

    Updated Code Tests

--- tests/services/test_graph_service.py
+++ tests/services/test_graph_service.py
@@ -1,63 +1,80 @@
 import datetime
-import logging
-import unittest
-
-import pytz
+from time import sleep
 
 from tests.base_test import BaseTest
 
-from communicator import db, app
+import json
 from communicator.models import Sample
+from communicator import db, app
+from communicator.api import admin
+from communicator.models.notification import Notification
+
+
 from communicator.services.graph_service import GraphService, daterange
 from datetime import date, timedelta
 import json
-
+import random
 class TestGraphService(BaseTest):
-    def populate_test_db(self):
-        days = 50
+    def test_populate_db(self):
+        samples = db.session.query(Sample).all()
+        self.assertEqual(0, len(samples))
+        days = 7 * 5
+        
         for day in daterange(date.today() - timedelta(days), date.today()):
-            day_string = day.strftime('%Y%m%d%H%M')
+            day_string = day.strftime("%d-%m-%y")
             bar_code = '000000111-' + day_string + '-5030'
-            _sample = Sample(barcode=bar_code, location = 50, station = 30, student_id = "000000111",
-                        computing_id = "abc12d")
-            _sample.date = day
+            _sample = Sample(barcode=bar_code,
+                    location= 50,
+                    station= 20,
+                    date=day,
+                    created_on=day,
+                    last_modified=datetime.datetime.now(),  # Note Modified date is later than created date.
+                    student_id="000000111",
+                    email="daniel.h.funk@gmail.com",
+                    result_code="12345",
+                    ivy_file="xxx",
+                    email_notified=True,
+                    text_notified=True)
             db.session.add(_sample)
         db.session.commit()
         samples = db.session.query(Sample).all()
         self.assertNotEqual(0, len(samples))
 
     def test_get_totals_by_hour(self):
+        self.test_populate_db()
         graph = GraphService() 
-        self.populate_test_db()
         graph.update_search_filters({
-            "start_date": datetime.date(2020,11,1),
-            "end_date": datetime.date(2020,11,1),
+            #"start_date": datetime.date(2019,11,1),
+            #"end_date": datetime.date(2021,11,1),
         })
         result = graph.get_totals_by_hour()
-        for location in result:
-            for station in location:
-                self.assertEqual(result[location][station], 0)
+        self.assertTrue(len(result) > 0)
+
+        self.assertEqual(result[50][20][-6], 36)
         
     def test_get_totals_by_day(self):
+        self.test_populate_db()
         graph = GraphService() 
-        self.populate_test_db()
         graph.update_search_filters({
-            "start_date": datetime.date(2020,11,1),
-            "end_date": datetime.date(2020,11,1),
+           
         })
         result = graph.get_totals_by_day()
+        self.assertTrue(len(result) > 0)
         for location in result:
-            for station in location:
-                self.assertEqual(result[location][station], 0)
+            for station in result[location]:
+                for day in result[location][station]:
+                    self.assertEqual(day, 1)
 
     def test_get_totals_by_weekday(self):
+        self.test_populate_db()
         graph = GraphService() 
-        self.populate_test_db()
         graph.update_search_filters({
-            "start_date": date.today() - timedelta(100),
-            "end_date": date.today(),
-            "location": "50"
+           
         })
         result = graph.get_totals_by_weekday()
+        self.assertTrue(len(result) > 0)
         print(result)
-        self.assertTrue(20 not in result)
+        for location in result:
+            for station in result[location]:
+                for weekday in result[location][station]:
+                    self.assertTrue((weekday == 5 or weekday == 6 ))
